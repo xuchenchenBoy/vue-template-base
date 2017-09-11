9dd81caa5c676dd25cf728764f1ff9e2a322347b
xuchen <xuchen@tangusoft.com>
2017-09-11 19:00:23 +0800

    add views

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="header">
+      header
+    </div>
+    <div>
+      content
+    </div>
+    <div>
+      footer
+    </div>
+  </div>
+</template>
+
+import Hello from '../components/Hello.vue';
+
+<script>
+  export default {
+    components: {
+   
+    }
+  }
+</script>
+
+<style scoped>
+  .header {
+    padding: 5px
+  }
+</style>
